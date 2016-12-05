059bf3ab6f9b36137c1f472f7722cf540312cb62
Rhoulhac <greg.rhoulhac@gmail.com>
2016-12-04 22:39:06 -0600

    NFL App, updates

--- stats/models.py
+++ stats/models.py
@@ -11,12 +11,12 @@ class Location(models.Model):
     }
 
     city = models.CharField(max_length=200)
-    state = models.CharField(max_length=2, choices=STATES)
+    state = models.CharField(max_length=4, choices=STATES)
+
+    #def __str__(self):
+        #return self.city
 
     def __str__(self):
-        return self.city
-
-    def __repr__(self):
         return '{} {}'.format(self.city, self.state)
 
 
@@ -26,7 +26,7 @@ class Team(models.Model):
     mascot = models.CharField(max_length=200)
     location = models.ForeignKey(Location)
 
-    def __repr__(self):
+    def __str__(self):
         return self.name
 
 
@@ -35,6 +35,9 @@ class Quaterback(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     team = models.ForeignKey(Team)
+    passing_yards = models.IntegerField(null=True)
+    touchdowns = models.IntegerField(null=True)
+    interceptions = models.IntegerField(null=True)
 
-    def __repr__(self):
+    def __str__(self):
         return '{} {}'.format(self.first_name, self.last_name)
